29ebc1de9540a154a037cac4b57fd3a0972b7eb2
Victor Zellmeister <victor.zellmeister@aldeia.io>
2021-03-23 21:47:00 -0300

    First working version. Still shit.
    Signed-off-by: Victor Zellmeister <victor.zellmeister@aldeia.io>

--- crawler/crawling_in_my_skin.py
+++ crawler/crawling_in_my_skin.py
@@ -2,16 +2,41 @@ from os import path
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
+DOMAIN = 'sindpd.org.br'
+FIRST_PAGE = 'https://sindpd.org.br/sindpd/site/'
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-driver = webdriver.Chrome(chrome_options=chrome_options, executable_path='/home/victor/auxinas/experiment_crawler/chromedriver')
+BROWSER = webdriver.Chrome(chrome_options=chrome_options, executable_path='/home/victor/auxinas/experiment_crawler/chromedriver')
+ALL_LINKS = []
+CRAWLED = []
 
-browser = driver.get('https://sindpd.org.br/sindpd/site/')
+def isInDomain(link):
+    print(link)
+    try:
+        return DOMAIN in link
+    except:
+        return
 
-links = driver.find_elements_by_tag_name('a')
+def isJspPage(link):
+    return '.jsp' in link.lower()
 
-print (links)
+def isDesiredDocument(link):
+    return not isJspPage(link) and isInDomain(link)
+
+def shouldCrawl(link):
+    return isInDomain(link) and isJspPage(link)
+
+def crawl(link):
+    BROWSER.get(link)
+    links = [link.get_attribute('href') for link in BROWSER.find_elements_by_tag_name('a')]
+    ALL_LINKS.extend(links)
+    linksToCrawl = [link for link in links if shouldCrawl(link)]
+    for link in [link for link in linksToCrawl if link not in CRAWLED]:
+        CRAWLED.append(link)
+        crawl(link)
+
+crawl(FIRST_PAGE)
+print(ALL_LINKS)
